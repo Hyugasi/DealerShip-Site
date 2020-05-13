863991348d1c9a139e22c73d77f790f253f0aa60
Hyugasi <isaiah.gordon@gmail.com>
2020-05-13 13:41:10 -0500

    tried to fix reload issue

--- frontend/admin/src/components/description/description.js
+++ frontend/admin/src/components/description/description.js
@@ -6,19 +6,23 @@ function Description(props) {
     const vehicleEdit = props.vehicles.filter((vehicle, key) => {
         return vehicle._id === props.match.params.id
     })
-    console.log('vehicleEdit', vehicleEdit)
 
-    return (
-        <div>
-            <h1>Details</h1>
-            <img src={vehicleEdit[0].img} />
-            <h1>{vehicleEdit[0].make}</h1>
-            <h2>{vehicleEdit[0].model}</h2>
-            <p>{vehicleEdit[0].description[0].description}</p>
-            <p>Condition: {vehicleEdit[0].description[0].condition}</p>
-        </div>
+    if (vehicleEdit === []) {
+        return (<div>Loading...</div>)
+    } else {
+        return (
+            <div>
+                <h1>Details</h1>
+                <img src={vehicleEdit[0].img} />
+                <h1>{vehicleEdit[0].make}</h1>
+                <h2>{vehicleEdit[0].model}</h2>
+                <p>{vehicleEdit[0].info[0].description}</p>
+                <p>Condition: {vehicleEdit[0].info[0].condition}</p>
+                <p>Mileage: {vehicleEdit[0].info[0].mileage}</p>
+            </div>
 
-    )
+        )
+    }
 }
 
 export default Description
