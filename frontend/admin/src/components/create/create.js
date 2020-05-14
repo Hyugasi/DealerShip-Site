78b4097138c143c596ca93bb155e19dfdefcea49
Hyugasi <isaiah.gordon@gmail.com>
2020-05-14 09:44:49 -0500

    got delete working and had create working

--- frontend/admin/src/components/create/create.js
+++ frontend/admin/src/components/create/create.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
 import { createVehicle } from '../../services/api-helper'
 
 function Create() {
@@ -6,10 +7,10 @@ function Create() {
     const [model, setModel] = useState("")
     const [img, setImg] = useState("")
     const [year, setYear] = useState("")
-    const [price, setPrice] = useState()
+    const [price, setPrice] = useState(undefined)
     const [description, setDescription] = useState("")
     const [condition, setCondition] = useState("")
-    const [mileage, setMileage] = useState()
+    const [mileage, setMileage] = useState(undefined)
 
 
     const handleCreate = async (e) => {
@@ -36,9 +37,50 @@ function Create() {
         setDescription("")
         setCondition("")
         setMileage("")
+        console.log(json)
     }
+    const handleMakeChange = (e) => {
+        setMake(e.target.value)
+    }
+    const handleModelChange = (e) => {
+        setModel(e.target.value)
+    }
+    const handleImgChange = (e) => {
+        setImg(e.target.value)
+    }
+    const handleYearChange = (e) => {
+        setYear(e.target.value)
+    }
+    const handlePriceChange = (e) => {
+        setPrice(e.target.value)
+    }
+    const handleDescChange = (e) => {
+        setDescription(e.target.value)
+    }
+    const handleCondititonChange = (e) => {
+        setCondition(e.target.value)
+    }
+    const handleMileageChange = (e) => {
+        setMileage(e.target.value)
+    }
+
     return (
-        <div>This is the Create Page</div>
+        <div>
+            <h1>Add a Vehicle</h1>
+            <form onSubmit={handleCreate}>
+                <input type="text" placeholder="Make" value={make} onChange={handleMakeChange} /><br />
+                <input type="text" placeholder="model" value={model} onChange={handleModelChange} /><br />
+                <input type="text" placeholder="img" value={img} onChange={handleImgChange} /><br />
+                <input type="text" placeholder="year" value={year} onChange={handleYearChange} /><br />
+                <input type="text" placeholder="price" value={price} onChange={handlePriceChange} /><br />
+                <input type="text" placeholder="description" value={description} onChange={handleDescChange} /><br />
+                <input type="text" placeholder="condition" value={condition} onChange={handleCondititonChange} /><br />
+                <input type="text" placeholder="mileage" value={mileage} onChange={handleMileageChange} /><br />
+                <Link to="/manage">
+                    <button onSubmit={handleCreate}>Submit</button>
+                </Link>
+            </form>
+        </div>
     )
 }
 
--- frontend/admin/src/components/description/description.js
+++ frontend/admin/src/components/description/description.js
@@ -1,30 +1,41 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
 import './style.css'
+import { deleteVehicle } from '../../services/api-helper'
 
 function Description(props) {
+    const [vehicles, setVehicles] = useState([])
 
     const vehicleEdit = props.vehicles.filter((vehicle, key) => {
         return vehicle._id === props.match.params.id
     })
-
-    if (vehicleEdit === []) {
-        return (<div>Loading...</div>)
-    } else {
-        return (
-            <div>
-                <h1>Details</h1>
-                <img src={vehicleEdit[0].img} />
-                <h1>{vehicleEdit[0].make}</h1>
-                <h2>{vehicleEdit[0].model}</h2>
-                <p>{vehicleEdit[0].info[0].description}</p>
-                <p>Condition: {vehicleEdit[0].info[0].condition}</p>
-                <p>Mileage: {vehicleEdit[0].info[0].mileage}</p>
-                <button>Delete</button>
-                <button>Edit</button>
-            </div>
-
-        )
+    const refreshPage = () => {
+        window.location.reload()
     }
+
+    const handleDelete = async (id) => {
+        const json = await deleteVehicle(id)
+        console.log("handleDelete", json)
+        const vehicleArr = vehicles.filter((vehicle) => vehicle._id !== id)
+        setVehicles(vehicleArr)
+        refreshPage()
+    }
+
+    return (
+        <div>
+            <h1>Details</h1>
+            <img src={vehicleEdit[0].img} />
+            <h1>{vehicleEdit[0].make}</h1>
+            <h2>{vehicleEdit[0].model}</h2>
+            <p>{vehicleEdit[0].info[0].description}</p>
+            <p>Condition: {vehicleEdit[0].info[0].condition}</p>
+            <p>Mileage: {vehicleEdit[0].info[0].mileage}</p>
+            <Link to="/manage">
+                <button onClick={() => handleDelete(vehicleEdit[0]._id)}>Delete</button>
+            </Link>
+            <button>Edit</button>
+        </div>
+    )
 }
 
 export default Description
